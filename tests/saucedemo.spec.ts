7e5cb1eae8c427f534be31c3dc654638ceb5fec4
R3nzoM3za <115664511+RenzoMezaValencia@users.noreply.github.com>
2025-09-12 12:00:05 -0500

    Test fix

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -6,8 +6,8 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Sign in flow', () => {
   test('should allow me to add a new user', async ({ page }) => {
-    page.getByPlaceholder('Username').fill('standard_user');
-    page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     await expect(page.getByText('Products')).toBeVisible();
